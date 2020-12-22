bab00ffc5df18e2ea243d80010880edbcb471bc5
磨相斌 <mxb919014877@163.com>
2020-12-22 18:01:56 +0800

    第二次尝试

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-
+    <main-tab-bar />
   </div>
 </template>
 
 <script>
-
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar.vue'
 
   export default {
     name: 'App',
     components: {
-      
+      MainTabBar
     }
   }
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router
+// 1.安装插件
+Vue.use(VueRouter)
+
+// 2.创建router
+const router = new VueRouter({
+
+})
+
+export default router
