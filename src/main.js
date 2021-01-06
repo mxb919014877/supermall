dbd41ec346ad9eb8c631b1bdbbfbf904faa5e9cc
磨相斌 <mxb919014877@163.com>
2021-01-06 10:11:28 +0800

    结束购物车模块

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue'
 import store from './store'
 import router from './router'
 
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
+
 import toast from 'components/common/toast'
 
 Vue.config.productionTip = false
@@ -10,6 +13,14 @@ Vue.config.productionTip = false
 Vue.prototype.$bus = new Vue()
 // 安装toast插件
 Vue.use(toast)
+
+// 解决移动端的300ms延迟
+FastClick.attach(document.body)
+
+// 使用懒加载的插件
+Vue.use(VueLazyLoad,{
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   store,
